23531c18f2a9803e240bd5badb1b0e6d9479d4a4
Ujas Devang Shah <ujasshah2006@gmail.com>
2025-08-26 21:38:48 +0530

    Enhance decision tree with real feature support

--- tree/base.py
+++ tree/base.py
@@ -20,6 +20,7 @@ class Node:
         self.value = value #for leaf node
         #for decision nodes
         self.feature = None
+        self.isreal_feature = False
         self.threshold = None
         self.left_child = None
         self.right_child = None
@@ -47,32 +48,54 @@ class DecisionTree:
             current_node = Node()
             current_node.feature = best_feature
             current_node.threshold = best_value
+            current_node.is_real_feature = check_ifreal(X[best_feature])
             X_left, y_left, X_right, y_right = split_data(X, y, best_feature, best_value)
             current_node.left_child = self._grow_tree(X_left, y_left, depth+1)
             current_node.right_child = self._grow_tree(X_right, y_right, depth+1)
             return current_node
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
-        """
-        Function to train and construct the decision tree
-        """
+        self.root = self._grow_tree(self, X, y)
 
         # If you wish your code can have cases for different types of input and output data (discrete, real)
         # Use the functions from utils.py to find the optimal attribute to split upon and then construct the tree accordingly.
         # You may(according to your implemetation) need to call functions recursively to construct the tree. 
 
-        pass
-
+    def _traverse_tree(self, row, node):
+        if (node.value is not None):
+            return node.value;
+        feature_value = row[node.feature]
+        if node.is_real_feature:
+            if feature_value <= node.threshold:
+                return self._traverse_tree(row, node.left_child)
+            return self._traverse_tree(row, node.right_child)
+        else:
+            if feature_value == node.threshold:
+                return self._traverse_tree(row, node.left_child)
+            return self._traverse_tree(row, node.right_child)
+    
     def predict(self, X: pd.DataFrame) -> pd.Series:
-        """
-        Funtion to run the decision tree on test inputs
-        """
+        predictions = X.apply(lambda row : self._traverse_tree(row, self.root), axis = 1)
+        return predictions
 
         # Traverse the tree you constructed to return the predicted values for the given test inputs.
 
-        pass
-
+    def _plot_recursive(self, node, indent = ""):
+        if node.value is not None:
+            print(indent + "--> Predict:", node.value)
+            return
+        if node.isreal_feature:
+            question = f"? ({node.feature} <= {node.threshold})"
+        else:
+            question = f"? ({node.feature} == {node.threshold})"
+        print(indent + question)
+        print (indent + "Y:")
+        self._plot_recursive(node.left_child, indent + "    ")
+        print(indent + "N:")
+        self._plot_recursive(node.right_child, indent + "    ")
+        
     def plot(self) -> None:
+        self._plot_recursive(self.root)
         """
         Function to plot the tree
 
