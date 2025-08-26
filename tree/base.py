e54c665e34160604c128f629c5debe12aa1adb93
Ujas Devang Shah <ujasshah2006@gmail.com>
2025-08-26 23:26:27 +0530

    Modify _grow_tree to use parent value for leaf nodes

--- tree/base.py
+++ tree/base.py
@@ -34,28 +34,29 @@ class DecisionTree:
         self.criterion = criterion
         self.max_depth = max_depth
 
-    def _grow_tree(self, X, y, depth = 0): #helper function, only for interanl function
+    def _grow_tree(self, X, y, parent_val, depth = 0): #helper function, only for interanl function
+        if len(y) == 0:
+            return Node(value=parent_val)
+        curr_val = y.mode()[0] if not check_ifreal(y) else y.mean()
         if (depth >= self.max_depth or len(y.unique())==1 or X.shape[0] < 2):
-            output_isreal = check_ifreal(y)
-            if output_isreal:
-                leaf_value = y.mean()
-            else: 
-                leaf_value = y.mode()[0]
-            return Node(value = leaf_value)
+            return Node(value = curr_val)
         else:
             features = list(X.columns)
             best_feature, best_value = opt_split_attribute(X, y, self.criterion, features)
+            if best_feature is None:
+                return Node(value = curr_val)
             current_node = Node()
             current_node.feature = best_feature
             current_node.threshold = best_value
             current_node.is_real_feature = check_ifreal(X[best_feature])
             X_left, y_left, X_right, y_right = split_data(X, y, best_feature, best_value)
-            current_node.left_child = self._grow_tree(X_left, y_left, depth+1)
-            current_node.right_child = self._grow_tree(X_right, y_right, depth+1)
+            current_node.left_child = self._grow_tree(X_left, y_left, curr_val, depth+1)
+            current_node.right_child = self._grow_tree(X_right, y_right, curr_val, depth+1)
             return current_node
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
-        self.root = self._grow_tree(X, y)
+        parent_val = y.mode()[0] if not check_ifreal(y) else y.mean()
+        self.root = self._grow_tree(X, y, parent_val)
 
         # If you wish your code can have cases for different types of input and output data (discrete, real)
         # Use the functions from utils.py to find the optimal attribute to split upon and then construct the tree accordingly.
