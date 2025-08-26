7a2f63c502859f021cc5f3cdb348c5c1941e4c79
Ujas Devang Shah <ujasshah2006@gmail.com>
2025-08-26 23:03:04 +0530

    Fix typo in one_hot_encoding function call

--- usage.py
+++ usage.py
@@ -59,7 +59,7 @@ N = 30
 P = 5
 X = pd.DataFrame({i: pd.Series(np.random.randint(P, size=N), dtype="category") for i in range(5)})
 y = pd.Series(np.random.randint(P, size=N), dtype="category")
-X = one_hot_enocding(X)
+X = one_hot_encoding(X)
 for criteria in ["information_gain", "gini_index"]:
     tree = DecisionTree(criterion=criteria)  # Split based on Inf. Gain
     tree.fit(X, y)
