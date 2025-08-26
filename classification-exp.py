fd0dea1a1864a38d7383d585ab02b3a59e3dd184
Ujas Devang Shah <ujasshah2006@gmail.com>
2025-08-27 00:23:16 +0530

    Implement Decision Tree training and evaluation

--- classification-exp.py
+++ classification-exp.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from tree.base import DecisionTree
 from metrics import *
 from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
 
 np.random.seed(42)
 
@@ -15,4 +16,19 @@ X, y = make_classification(
 plt.scatter(X[:, 0], X[:, 1], c=y)
 
 # Write the code for Q2 a) and b) below. Show your results.
+
+#Q2 a)
+
+X_df = pd.Dataframe(X)
+y_s = pd.Series(y)
+X_train, X_test, y_train, y_test = train_test_split(X_df, y_s, test_size = 0.3, random_state = 07)
+tree = DecisionTree(criterion = "information_gain", max_depth = 5)
+tree.fit(X_train, y_train)
+y_hat = tree.predict(X_test)
+print("Results for Part(a)")
+print(f"Accuracy : {accuracy(y_hat, y_test):.2f}")
+for cls in y_test.unique():
+    p = precision(y_hat, y_test, cls)
+    r = recall(y_hat, y_test, cls)
+    print(f"Class {cls} - Precision: {p:.2f}, Recall: {r.2f}") 
 
