61bbc2d45112c5f5f027c7896e4af3a62bbd6365
Ujas Devang Shah <ujasshah2006@gmail.com>
2025-08-26 22:00:20 +0530

    Add assert checks and implement metrics functions

--- metrics.py
+++ metrics.py
@@ -1,46 +1,34 @@
 from typing import Union
 import pandas as pd
+import numpy as np
 
+"""Students are required to add appropriate assert checks at places to
+    ensure that the function does not fail in corner cases."""
 
 def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
     """
-    Function to calculate the accuracy
-    """
-
-    """
     The following assert checks if sizes of y_hat and y are equal.
-    Students are required to add appropriate assert checks at places to
-    ensure that the function does not fail in corner cases.
     """
     assert y_hat.size == y.size
-    # TODO: Write here
-    pass
+    return (y_hat == y).sum() / y.count()
 
 
 def precision(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
-    """
-    Function to calculate the precision
-    """
-    pass
+    assert y_hat.size == y.size
+    if ((y_hat == cls).sum() == 0): return 0.0
+    return ((y_hat == cls) & (y == cls)).sum() / (y_hat == cls).sum()
 
 
 def recall(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
-    """
-    Function to calculate the recall
-    """
-    pass
-
+    assert y_hat.size == y.size
+    if ((y == cls).sum() == 0): return 0.0
+    return ((y_hat == cls) & (y == cls)).sum() / (y == cls).sum()
 
 def rmse(y_hat: pd.Series, y: pd.Series) -> float:
-    """
-    Function to calculate the root-mean-squared-error(rmse)
-    """
-
-    pass
+    assert y_hat.size == y.size
+    return np.sqrt(((y_hat - y) ** 2).mean())
 
 
 def mae(y_hat: pd.Series, y: pd.Series) -> float:
-    """
-    Function to calculate the mean-absolute-error(mae)
-    """
-    pass
+    assert y_hat.size == y.size
+    return (np.abs(y_hat - y)).mean()
