5de9f1309b005c4ec2c0273c132d6b38a19a7495
Ujas Devang Shah <ujasshah2006@gmail.com>
2025-08-27 23:37:26 +0530

    Add data preprocessing and decision tree model comparison
    Implemented data cleaning and decision tree comparison.

--- auto-efficiency.py
+++ auto-efficiency.py
@@ -14,3 +14,33 @@ data = pd.read_csv(url, delim_whitespace=True, header=None,
 
 # Clean the above data by removing redundant columns and rows with junk values
 # Compare the performance of your model with the decision tree module from scikit learn
+
+data.drop('car name', inplace = True, axis = 1) #car name is of no use to our predictions
+#dropping the rows with ? in horsepower since they are very few and would be a problem for fitting our tree
+data['horsepower'] = data['horsepower'].replace('?', np.nan)
+data = data.dropna()
+#since origin is a categorical feature, we one hot encode it
+data = pd.get_dummies(data, columns = ['origin'], prefix = ['origin'])
+#defining the feature-data and the output or target variable
+y = data['mpg']
+X = data.drop('mpg', axis = 1)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 7)
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error
+
+print("My Decision Tree")
+my_tree = DecisionTree(criterion = 'information_gain', max_depth = 5)
+my_tree.fit(X_train, y_train)
+my_y_hat = my_tree.predict(X_test)
+my_rmse = rmse(my_y_hat, y_test)
+print(f"My Decision Tree's RMSE = {my_rmse:.2f}")
+
+print("Scikitlearn's Decision Tree")
+sklearn_tree = DecisionTreeRegressor(max_depth = 5, random_state = 7)
+sklearn_tree.fit(X_train, y_train)
+sklearn_y_hat = sklearn_tree.predict(X_test)
+sklearn_rmse = np.sqrt(mean_squared_error(sklearn_y_hat, y_test))
+print(f"Scikitlearn's Decision Tree's RMSE = {sklearn_rmse:.2f}")
